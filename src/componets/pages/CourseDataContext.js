d701390348b9b0f7dcf8b0c3b758460e296ee42f
Sarath O S <sarathos584@gmail.com>
2023-07-05 23:38:45 +0530

    reviews listed successfully

--- src/componets/pages/CourseDataContext.js
+++ src/componets/pages/CourseDataContext.js
@@ -3,6 +3,12 @@ import React,{ createContext, useState } from "react";
 export const CourseDataContext = createContext()
 
 export const CourseDataProvider = ({children})=>{
+  const [reviews,setReviews] = useState([
+    {id:1,reviwer:'Sarath',rating:4,courseName:'MERN stack',review:'The course they offer worth learning for future endevours'},
+    {id:2,reviwer:'San T',rating:4,courseName:'MEAN stack',review:'This course from limenzy made me join as a fersher developer with a 10 LPA'},
+    {id:3,reviwer:'Rimaz',rating:4,courseName:'Python Django',review:'Python has the power for many things that other languages usually does not offer, so this course was helpfull for my carrier kickstart as a software developer'},
+    {id:4,reviwer:'Basil Jiji',rating:4,courseName:'Ruby Rails',review:'Even though Ruby is a comparitively newer language, there is a better future and career waiting for you'},
+  ])
     const [courses, setCourses] = useState([
         { id: 1, courseName: "Python Django", rating: 4, placementRate: "89%" },
         { id: 2, courseName: "Ruby on Rails", rating: 3, placementRate: "86%" },
@@ -14,7 +20,7 @@ export const CourseDataProvider = ({children})=>{
         { id: 8, courseName: "Machine Learning", rating: 5, placementRate: "92%" },
       ]);
       return (
-        <CourseDataContext.Provider value={{courses,setCourses}}>
+        <CourseDataContext.Provider value={{courses,setCourses,reviews,setReviews}}>
             {children}
         </CourseDataContext.Provider>
       )
--- src/componets/pages/home/Home.js
+++ src/componets/pages/home/Home.js
@@ -1,9 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./home.css";
 import { CourseDataContext } from "../CourseDataContext";
 
 const Home = () => {
-  const { courses, setCourses } = useContext(CourseDataContext);
+  // const [reviewer,setReviewer] = useState('');
+  // const 
+  const { courses, setCourses, reviews, setReviews } =
+    useContext(CourseDataContext);
   const sortedCourses = courses.sort((a, b) => b.rating - a.rating);
   const topTwoCourses = sortedCourses.slice(0, 2);
   return (
@@ -15,23 +18,48 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="top-courses container-fluid">
+      <section className="top-courses container-fluid py-5">
         <div className="container">
           <div className="row text-center">
-            <h1>Our Top Rated Courses</h1>
-            {
-                topTwoCourses.map(course=>{
-                    return (
-                        <div className="card col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                            <h1>{course.courseName}</h1>
-                            <p>rating: {course.rating}</p>
-                            <p>placement rate : {course.placementRate}</p>
-                        </div>
-                    )
-                })
-            }
+            <h1 className="mb-5">Our Top Rated Courses</h1>
+            {topTwoCourses.map((course) => {
+              return (
+                <div className="cards col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                  <div className="card px-3 py-4">
+                    <h1>{course.courseName}</h1>
+                    <p>rating: {course.rating}</p>
+                    <p>placement rate : {course.placementRate}</p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
+      </section>
+      <section className="container-fluid reviews py-5">
+        <div className="container text-center">
+          <h1 className="mb-5">User reivews</h1>
+          <div className="row user-reviews">
+            {reviews.map((review) => {
+              return (
+                <div className="cards col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                  <div className="card-wrap px-3 py-4">
+                    <p> user name : {review.reviwer}</p>
+                    <p>{review.courseName}</p>
+                    <p>{review.review}</p>
+                    <p>rating : {review.rating}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="review-form">
+            <form action="">
+              <input type="text" />
+            </form>
+          </div>
+        </div>
+
       </section>
     </>
   );
